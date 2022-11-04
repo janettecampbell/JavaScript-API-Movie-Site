2f333ae3754eaec82364f57068242c32da42738e
Janette Campbell <campbjp@hotmail.com>
2022-11-04 15:02:50 -0400

    chore: set up localhost port

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const helmet = require("helmet");
 const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// ============== Root route for the app ====================
+
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to my app");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}...`);
+});
